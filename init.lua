ce718f59048fffbb4112fde5951ed63db6c03ba9
Somtoo Chukwurah <chukwurahb@gmail.com>
2025-07-12 21:36:25 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -219,7 +219,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
         vim.hl.on_yank()
     end,
 })
-
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
@@ -909,12 +908,11 @@ require('lazy').setup({
             signature = { enabled = true },
         },
     },
-
     { -- You can easily change to a different colorscheme.
-        -- Change the name of the colorscheme plugin below, and then
-        -- change the command in the config to whatever the name of that colorscheme is.
-        --
-        -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+        --     -- Change the name of the colorscheme plugin below, and then
+        --     -- change the command in the config to whatever the name of that colorscheme is.
+        --     --
+        --     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
         'folke/tokyonight.nvim',
         priority = 1000, -- Make sure to load this before all the other start plugins.
         config = function()
@@ -923,6 +921,7 @@ require('lazy').setup({
                 styles = {
                     comments = { italic = false }, -- Disable italics in comments
                 },
+                transparent = true,
             }
 
             -- Load the colorscheme here.
